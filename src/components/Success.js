1619ff415230731d46e211268d84f292457f0ecb
Shrinath Nayak <shrinathnayak07@gmail.com>
2018-11-07 02:27:09 +0530

    Removed Redirection

--- src/components/Success.js
+++ src/components/Success.js
@@ -2,27 +2,11 @@ import React, { Component } from 'react'
 import { Alert } from 'reactstrap';
 
 export class Success extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      redirectTime: 5
-    }
-  }
-  componentDidMount(){
-    window.setTimeout( () => {
-      window.location.href = '/'
-    }, 5000);
-    
-    setInterval(() => {
-      this.setState({ redirectTime: this.state.redirectTime - 1 })
-    }, 1000);
-  }
   render() {
-    const { redirectTime } = this.state;
     return (
       <Alert color="success" style={styles.main}>
         <h3>Success</h3>
-        <p className="muted">User Data is been submitted successfully. <br /> Redirecting back to home page in {redirectTime} sec.</p>
+        <p className="muted">User Data is been submitted successfully.</p>
       </Alert>
     )
   }
