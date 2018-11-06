17a0e6033ecf840dea2c7515ef731ac289a27762
Shrinath Nayak <shrinathnayak07@gmail.com>
2018-11-07 02:16:55 +0530

    Updates

---
+++ precache-manifest.e0accf6430b1cc098dd767fded7da583.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "db34dbf98a0d7e11cf2c",
+    "url": "/react-multiform/static/js/runtime~main.db34dbf9.js"
+  },
+  {
+    "revision": "fc770318ac6a4ce9e105",
+    "url": "/react-multiform/static/js/main.fc770318.chunk.js"
+  },
+  {
+    "revision": "70d911f0abad7ccf7845",
+    "url": "/react-multiform/static/js/1.70d911f0.chunk.js"
+  },
+  {
+    "revision": "fc770318ac6a4ce9e105",
+    "url": "/react-multiform/static/css/main.3073b0b9.chunk.css"
+  },
+  {
+    "revision": "70d911f0abad7ccf7845",
+    "url": "/react-multiform/static/css/1.d5343496.chunk.css"
+  },
+  {
+    "revision": "53b11a682af84a273075d0e6a7c97ba9",
+    "url": "/react-multiform/index.html"
+  }
+];
